a9cbe9100986c13ceb35938b9a9c6ad6a1cad51d
smartdev <winewater301@gmail.com>
2016-12-07 15:23:36 -0500

    implement using angular service

--- public/javascripts/controllers/user.controller.js
+++ public/javascripts/controllers/user.controller.js
@@ -1,84 +1,85 @@
 'use strict';
 
-mainApp.controller('UserController', ['$scope', '$http', '$stateParams', '$state', '$window', function($scope, $http, $stateParams, $state, $window) {
+mainApp.controller('UserController', ['$scope', '$http', '$stateParams', '$state', '$window', 'UserService', '$log', function($scope, $http, $stateParams, $state, $window, UserService, $log) {
   // variables
   $scope.users = [];
   $scope.pageState = $stateParams.pageState;
 
   // functions
   function getUsers() {
-    $http.get('/api/v1/users').then(
+    UserService.getUsers().then(
       function(users) {
-        console.log(users);
+        $log.log(users);
         $scope.users = users;
       },
       function(error) {
-        console.log(error);
+        $log.error(error);
       });
   }
 
   function addUser() {
-    $http.post('/api/v1/users/create', $scope.user).then(
+    UserService.addUser($scope.user).then(
       function(user) {
-        console.log('addUser');
-        console.log(user);
+        debugger;
+        $log.log(user);
         $scope.pageState = "users";
         init();
-        $state.go('users.list', {reload: true});
+        $state.go("users.list", {reload: true});
       },
       function(error) {
-        console.log(error);
+        $log.error(error);
       });
   }
 
   function init() {
-    console.log('init');
     getUsers();
   }
 
   function updateUser(id) {
-    $http.put('/api/v1/users/' + id, $scope.user).then(
+    UserService.updateUser(id, $scope.user).then(
       function(user) {
-        console.log('updateUser');
-        console.log(user);
+        debugger;
+        $log.log(user);
         $scope.pageState = "users";
-        $state.go('users.list');
+        $state.go("users.list");
       },
       function(error) {
-        console.log(error);
+        $log.log(error);
       });
   }
 
   function editUser(id) {
-    $http.get('/api/v1/users/' + id).then(
+    UserService.editUser(id).then(
       function(user) {
-        console.log(user);
-        $scope.user = user.data;
+        debugger;
+        $log.log(user);
+        $scope.user = user;
         $scope.pageState = "users.edit";
       },
       function(error) {
-        console.log(error);
-      });
+        $log.log(error);
+      })
   }
 
   function deleteUser(id) {
     var deleteUser = $window.confirm('Are you sure you want to delete the Ad?');
     if (deleteUser) {
-      $http.delete('/api/v1/users/' + id).then(
-      function(user) {
-        getUsers();
-      },
-      function(error) {
-        console.log(error);
-      });
+      UserService.deleteUser(id).then(
+        function(user) {
+          debugger;
+          getUsers();
+        },
+        function(error) {
+          $log.log(error);
+        });
     }
-
   }
 
-  $scope.init = init;
   $scope.getUsers = getUsers;
   $scope.addUser = addUser;
   $scope.updateUser = updateUser;
   $scope.editUser = editUser;
   $scope.deleteUser = deleteUser;
+
+  init();
 }]);
--- public/javascripts/services/user.service.js
+++ public/javascripts/services/user.service.js
@@ -1,15 +1,59 @@
-myApp.factory('userService', function($http, $log, $q) {
+mainApp.factory('UserService', function($http, $log, $q) {
   return {
     getUsers: function() {
       var deferred = $q.defer();
       $http.get('/api/v1/users/')
-       .success(function(users) {
+        .success(function(users) {
           deferred.resolve(users);
-       }).error(function(msg, code) {
+        }).error(function(msg, code) {
           deferred.reject(msg);
           $log.error(msg, code);
-       });
+        });
      return deferred.promise;
+    },
+    addUser: function(user) {
+      var deferred = $q.defer();
+      $http.post('/api/v1/users/create', user)
+        .success(function(user) {
+          deferred.resolve(user);
+        }).error(function(msg, code) {
+          deferred.reject(msg);
+          $log.error(msg, code);
+        });
+      return deferred.promise;
+    },
+    updateUser: function(id, user) {
+      var deferred = $q.defer();
+      $http.put('/api/v1/users/' + id, user)
+        .success(function(user) {
+          deferred.resolve(user);
+        }).error(function(msg, code) {
+          deferred.reject(msg);
+          $log.error(msg, code);
+        });
+      return deferred.promise;
+    },
+    editUser: function(id) {
+      var deferred = $q.defer();
+      $http.get('/api/v1/users/' + id)
+        .success(function(user) {
+          deferred.resolve(user);
+        }).error(function(msg, code) {
+          deferred.reject(msg);
+          $log.error(msg, code);
+        });
+      return deferred.promise;
+    },
+    deleteUser: function(id) {
+      var deferred = $q.defer();
+      $http.delete('/api/v1/users/' + id)
+        .success(function(user) {
+          deferred.resolve(user);
+        }).error(function(msg, code) {
+          deferred.reject(msg);
+          $log.error(msg, code);
+        });
+      return deferred.promise;
     }
   }
 });
